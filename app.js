b5610c4a423dbb2e037321c8a5cbf52295299a6a
danutdotrar <morarasu.danut@gmail.com>
2022-08-11 18:55:43 +0300

    Solved selection problem

--- app.js
+++ app.js
@@ -1,63 +1,71 @@
-const item = ["rock", "paper", "scissors"]
+var playerScore = 0;
+var computerScore = 0;
 
-var tie = 0;
-var player = 0;
-var computer = 0;
+const buttons = document.querySelectorAll('#button');
 
 // Return random computer item - Rock / Paper / Scissors
 function computerPlay() {
-    return item[Math.floor(Math.random() * item.length)];
+    let item = ["rock", "paper", "scissors"]
+    return item[Math.floor(Math.random() * item.length)]
 }
 
-// Takes a valid input from the user
-// let playerSelection = prompt("Type in your figher: rock, paper or scissors").toLowerCase();
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
 
 // Play round and count them
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        tie++;
-        return console.log("It's a tie!");
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        player++;
-        return console.log("Player wins! Rock beats scissors!");
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        player++;
-        return console.log("Player wins! Paper beats rock!");
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        player++;
-        return console.log("Player wins! Scissors beats paper!");
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        computer++;
-        return console.log("Computer wins! Rock beats scissors!");
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        computer++;
-        return console.log("Computer wins! Paper beats rock!");
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        computer++;
-        return console.log("Computer wins! Scissors beat paper");
-    } else {
-        console.log("You need to choose from rock, paper or scissors");
-    }
-}
-
-// Final result comparision
-function result(){
-    if ((tie > player) && (tie > computer)){
-        console.log("Final result - It's a tie!")
-    } else if (player > computer){
-        console.log("Final result - Player win! :)")
-    } else {
-        console.log("Final result - Player lose! :(")
-    }
-}
-
-// Play the game for 5 rounds
-// for (let count = 0; count < 5; count++) {
-//     let computerSelection = computerPlay();
-//     console.log("Computer plays:" + computerSelection);
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
+    let result = ""
+    let playerScoreView = ""
+    let computerScoreView = ""
+    let finalResult = ""
     
-//     // Call function to play round of game
-//     playRound(playerSelection, computerSelection);
-// }
+    //Player wins
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+       (playerSelection == 'scissors' && computerSelection == 'paper') ||
+       (playerSelection == 'paper' && computerSelection =='rock')) {
 
-result();
+        playerScore += 1
+
+        result = ('Player win! ' + playerSelection + ' beats ' + computerSelection)
+
+        if (playerScore == 5) {
+            finalResult = ("Player wins the game! Reload the page to play again.")
+            disableButtons()
+        }
+    }
+
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie! You both choose ' + playerSelection)
+    }
+
+    else {
+        computerScore += 1
+        result = ('Player lose! ' + computerSelection + ' beats ' + playerSelection)
+
+        if (computerScore == 5) {
+            finalResult = ("Computer wins the game! Reload the page to play again")
+            disableButtons()
+        }
+    }
+
+    playerScoreView = ("Player Score: " + playerScore)
+    computerScoreView = ("Computer score: " + computerScore)
+
+    document.querySelector('.result').textContent = result
+    document.querySelector('.player-score').textContent = playerScoreView
+    document.querySelector('.computer-score').textContent = computerScoreView
+    document.querySelector('.final-result').textContent = finalResult
+    return
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
+
+
